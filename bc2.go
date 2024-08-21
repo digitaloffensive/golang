6c9defb489f0527e647dd408b859abdd5abeb490
Michael LaSalvia <genxweb@gmail.com>
2024-08-21 13:16:47 -0400

    Update bc2.go
    added hidewindow

--- bc2.go
+++ bc2.go
@@ -11,9 +11,11 @@ import (
         "io/ioutil"
         "net/http"
         "time"
+        "syscall"
 )
 
 func main() {
+        hideWindow()
         // Seed the random number generator with the current time
         rand.Seed(time.Now().UnixNano())
 
